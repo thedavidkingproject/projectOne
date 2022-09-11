5727f265c0f6dbf3e8315cacb27ef05a7d164e7c
thedavidkingproject <javicelis20@gmail.com>
2022-09-11 04:17:52 -0500

    yt

---
+++ YouTube.py
@@ -0,0 +1,12 @@
+Sub_Count = 800000
+MySubCount = input("What is your sub count, nigger? ")
+
+def YouTube_Calculator():
+    if int(MySubCount) == Sub_Count:
+        print("You're good.")
+    elif int(MySubCount) < Sub_Count:
+        print("You're getting there, pal.")
+    elif int(MySubCount) > Sub_Count:
+        print("I'll suck your dick..")
+
+YouTube_Calculator()
