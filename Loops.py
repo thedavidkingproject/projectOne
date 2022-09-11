8d26cab6c928d50ce5d77a18537956d1264bd29d
thedavidkingproject <javicelis20@gmail.com>
2022-09-11 03:40:11 -0500

    Update

---
+++ Loops.py
@@ -0,0 +1,9 @@
+#while
+x=0
+while (x<5):
+    print(x)
+    x=x+1
+
+#For
+for x in range(5,10):
+    print(x)
---
+++ main.py
@@ -0,0 +1,10 @@
+Thomas_Age = 3
+Age_at_Kindergarden = 5
+
+if Thomas_Age == Age_at_Kindergarden:
+    print("Thomas should be in kindergarden")
+elif Thomas_Age < Age_at_Kindergarden:
+    print("He should be in pre-school")
+elif Thomas_Age > Age_at_Kindergarden:
+    print("He should be in a higher grade")
+
